89cacaceff1a7d86b6c7e13c406606f7fa4159d0
CristopherVidalMachado <cristophervidal3@gmail.com>
2019-04-09 08:33:51 -0300

    Zerando os vetores RGB

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ pi2 = cmath.pi * 2.0
 def DFT2D(image):
     global M, N
     (M, N) = image.size # (tamanho em x, y)
+    dft2d_red = [[0.0 for k in range(M)] for l in range(N)] 
+    dft2d_grn = [[0.0 for k in range(M)] for l in range(N)] 
+    dft2d_blu = [[0.0 for k in range(M)] for l in range(N)] 
 
     return (dft2d_red, dft2d_grn, dft2d_blu)
         
