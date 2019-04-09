2127b98791213c7e462de373a42fbf3140441a13
CristopherVidalMachado <cristophervidal3@gmail.com>
2019-04-09 08:31:21 -0300

    Adicionando abertura da imagem e definindo escopo e retorno das functions

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from PIL import Image
 import cmath
 pi2 = cmath.pi * 2.0
+
+def DFT2D(image):
+    global M, N
+    (M, N) = image.size # (tamanho em x, y)
+
+    return (dft2d_red, dft2d_grn, dft2d_blu)
+        
+def IDFT2D(dft2d):
+
+    return image
+
+# TEST
+# Abre a imagem
+imgOpen = Image.open("horizontal_lines.jpg)
