681221a4948e98d8e650ddf088f641dbca05a563
Cristopher Vidal Machado <cristophervidal3@gmail.com>
2019-04-11 19:41:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ pi2 = cmath.pi * 2.0
 
 def DFT2D(image):
     global M, N
-    (M, N) = image.size # (tamanho em x, y)
-    dft2d_red = [[0.0 for k in range(M)] for l in range(N)] 
-    dft2d_grn = [[0.0 for k in range(M)] for l in range(N)] 
-    dft2d_blu = [[0.0 for k in range(M)] for l in range(N)] 
-
-    return (dft2d_red, dft2d_grn, dft2d_blu)
-        
-def IDFT2D(dft2d):
-
-    return image
-
-# TEST
-# Abre a imagem
-imgOpen = Image.open("horizontal_lines.jpg)
-from PIL import Image
-import cmath
-pi2 = cmath.pi * 2.0
-
-def DFT2D(image):
-    global M, N
     (M, N) = image.size # (imgx, imgy)
     dft2d_red = [[0.0 for k in range(M)] for l in range(N)] 
     dft2d_grn = [[0.0 for k in range(M)] for l in range(N)] 
