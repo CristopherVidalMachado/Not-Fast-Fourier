0e75ddd1d74dc818165e796718d55d2292be2778
Cristopher Vidal Machado <cristophervidal3@gmail.com>
2019-04-18 19:20:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,78 @@
 from PIL import Image
 import cmath
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
 pi2 = cmath.pi * 2.0
 
 def DFT2D(image):
     global M, N
-    (M, N) = image.size # (imgx, imgy)
-    dft2d_red = [[0.0 for k in range(M)] for l in range(N)] 
-    dft2d_grn = [[0.0 for k in range(M)] for l in range(N)] 
-    dft2d_blu = [[0.0 for k in range(M)] for l in range(N)] 
+    (M, N) = image.size
+    dft2d = [[0.0 for k in range(M)] for l in range(N)]
     pixels = image.load()
     for k in range(M):
+        print(k)
         for l in range(N):
-            sum_red = 0.0
-            sum_grn = 0.0
-            sum_blu = 0.0
+            sum = 0.0
             for m in range(M):
                 for n in range(N):
-                    (red, grn, blu, alpha) = pixels[m, n]
+                    (L) = pixels[m, n]
                     e = cmath.exp(- 1j * pi2 * (float(k * m) / M + float(l * n) / N))
-                    sum_red += red * e
-                    sum_grn += grn * e
-                    sum_blu += blu * e
-            dft2d_red[l][k] = sum_red / M / N
-            dft2d_grn[l][k] = sum_grn / M / N
-            dft2d_blu[l][k] = sum_blu / M / N
-    return (dft2d_red, dft2d_grn, dft2d_blu)
-        
+                    sum += L * e
+            dft2d[l][k] = sum / M / N
+    return (dft2d)
+
 def IDFT2D(dft2d):
-    (dft2d_red, dft2d_grn, dft2d_blu) = dft2d
+    (dft2d) = dft2d
     global M, N
-    image = Image.new("RGB", (M, N))
-    pixels = image.load() 
+    image = Image.new("L", (M, N))
+    pixels = image.load()
     for m in range(M):
+        print(m)
         for n in range(N):
-            sum_red = 0.0
-            sum_grn = 0.0
-            sum_blu = 0.0
+            sum = 0.0
             for k in range(M):
                 for l in range(N):
                     e = cmath.exp(1j * pi2 * (float(k * m) / M + float(l * n) / N))
-                    sum_red += dft2d_red[l][k] * e
-                    sum_grn += dft2d_grn[l][k] * e
-                    sum_blu += dft2d_blu[l][k] * e
-            red = int(sum_red.real + 0.5)
-            grn = int(sum_grn.real + 0.5)
-            blu = int(sum_blu.real + 0.5)
-            pixels[m, n] = (red, grn, blu)
+                    sum += dft2d[l][k] * e
+            L = int(sum.real+ 0.5)
+            pixels[m, n] = (L)
     return image
 
-# TEST
 
-image = IDFT2D(DFT2D(Image.open("input.png")))
+
+
+
+def filtro(dft2d):
+    (dft2d) = dft2d
+    global M, N
+    dft2dTemp =[[0.0 for k in range(M)] for l in range(N)]
+
+    for m in range(M):
+        print(m)
+        for n in range(N):
+            sum = dft2d[n][m]
+
+            if sum.real < 0:
+                sum.real = 0
+            dft2dTemp[n][m]=  sum
+    return dft2dTemp
+
+imageFrequency = DFT2D(Image.open("input.png"))
+
+# imageFiltrada  = filtro(imageFrequency)
+
+
+image = IDFT2D(imageFrequency)
+
+
+
 image.save("output.png", "PNG")
+print(image)
+dft_shift =(np.fft.fftshift(imageFrequency))
+magnitude_spectrum = 10*np.log(np.abs(dft_shift))
+magnitude_spectrum = np.asarray(magnitude_spectrum, dtype=np.uint8)
+cv2.imshow("saida.png", magnitude_spectrum)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
